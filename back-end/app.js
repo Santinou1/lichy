46a12f1c81cfecba2cf59a75d4337d9ec2792b22
Santinou1 <santitech445@gmail.com>
2025-06-11 16:26:15 -0300

    cors

--- back-end/app.js
+++ back-end/app.js
@@ -17,7 +17,16 @@ const historial = require('./routes/Historial.js');
 const pedidos = require('./routes/pedidos.js');
 const facturas = require('./routes/facturas.js');
 
-app.use(cors());
+// Configuración de CORS
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://192.168.0.131:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
