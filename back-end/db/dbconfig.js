ebaf7abafc8a19c77e294577d3575d8abcbb901f
Santinou1 <santitech445@gmail.com>
2025-06-11 11:47:30 -0300

    changeNames

--- back-end/db/dbconfig.js
+++ back-end/db/dbconfig.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: '127.0.0.1',
     port: '3306',
-    user: 'root',
-    password: '',
+    user: 'lichy',
+    password: '123456',
     database: 'lichydb'
 });
 
