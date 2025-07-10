3aba829d86936964486f009ebb105710599ba295
santinou1 <santitech445@gmail.com>
2025-07-10 17:16:00 -0300

    fix

---
+++ back-end/ecosystem.config.cjs
@@ -0,0 +1,12 @@
+// C:\Users\agadmin\Desktop\GestionLichy\lichy\back-end\ecosystem.config.cjs
+module.exports = {
+  apps: [
+    {
+      name: 'backendLichy',
+      script: 'cmd.exe',
+      args: '/c npm run dev',
+      cwd: 'C:/Users/agadmin/Desktop/GestionLichy/lichy/back-end',
+      interpreter: 'none',
+    }
+  ]
+};
---
+++ front-end/ecosystem.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'frontLichy',
+      script: 'cmd.exe',
+      args: '/c npm run dev-host',
+      interpreter: 'none'
+    }
+  ]
+};
