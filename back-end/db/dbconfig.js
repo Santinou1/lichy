2744256044edc8b699af96293a108d5cd7e577ac
Santinou1 <santitech445@gmail.com>
2025-07-10 15:40:35 -0300

    dbProd

--- back-end/db/dbconfig.js
+++ back-end/db/dbconfig.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+/* const pool = mysql.createPool({
     host: 'localhost',
     port: '3306',
     user: 'root',
     password: '',
+    database: 'lichydb'
+}); */
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'P1r1d3gm1@',
     database: 'lichydb'
 });
 
